94e5dfd6d1ffbd2a8bf601b4933ca2c00f2f9cab
Lucimeri Andretta <luandretta@hotmail.com>
2022-11-21 07:55:03 +0000

    Display additon question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,16 +9,30 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked submit");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
+
 })
 
-function runGame() {
+/**
+ * The main game "loop", called when the script is first loaded 
+ * and after the user's answer has been processed
+ */
+function runGame(gameType) {
     //Creates two random numbers between 1 and 25, +1 because the initial number is zero, floor to be intenger
     let num1 = Math.floor(Math.random() * 24) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}.Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -37,8 +51,11 @@ function incrementWrongAnswer() {
     
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
     
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand1;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
